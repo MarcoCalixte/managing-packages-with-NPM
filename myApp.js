5bcb847736c9a2ceb2acaea4e413bf84f0962656
marcorelcalixte <marcorelcalixte@gmail.com>
2022-07-27 15:17:46 -0400

    Add json Route wth dummy data

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+//Meet the node console
 let express = require('express');
 let app = express();
 var bGround = require('fcc-express-bground');
@@ -5,16 +6,21 @@ var bGround = require('fcc-express-bground');
 bGround.log('Hello World')
 console.log('Hello World')
 
-
+//serve an HTML file
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/views/index.html")
 })
 
+//serve static assets
 app.use(express.static(__dirname + "/public"))
 app.use('/public', express.static(__dirname + "/public")) // add static for assets
 
-
-
+// serve json on a specific route
+app.get("/json", function (req, res) {
+    res.json({
+        "message": "Hello json"
+    })
+})
 
 
 
